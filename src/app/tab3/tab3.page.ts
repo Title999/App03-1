f6bb6d4d17c2bf6e5ea13e493083ed562b018b08
paripat meenoy <paripatpat19@gmail.com>
2024-08-13 14:22:21 +0700

    first commit

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonButton, IonModal, IonSearchbar, IonList, IonAvatar, IonImg, IonButtons } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+  standalone: true,
+  imports: [IonButtons, IonImg, IonAvatar, IonList, IonSearchbar, IonModal, IonButton, IonLabel, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+})
+export class Tab3Page {
+  presentingElement = undefined;
+  constructor(private actionSheetCtrl: ActionSheetController) {}
+  isModalOpen = false;
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+  canDismiss = async () => {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Yes',
+          role: 'confirm',
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    actionSheet.present();
+
+    const { role } = await actionSheet.onWillDismiss();
+
+    return role === 'confirm';
+  };
+}
